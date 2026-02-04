41c9705d4567d37505a58a3587c511c44c4ebfc2
enjoys <deadly.info@gmail.com>
2026-02-04 10:10:54 +0300

    move global css

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,3 @@
-<script setup>
-</script>
 <template>
   <router-view />
 </template>
-
-<style>
-body {
-  min-width: 370px;
-}
-
-.v-input__details {
-  flex-direction: column-reverse;
-  padding-inline: 0 !important;
-  align-items: start !important;
-}
-</style>
