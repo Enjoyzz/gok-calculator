6fe42bcad23e934d58bc0fcd8ea10e5db0e7c267
enjoys <deadly.info@gmail.com>
2026-02-08 20:52:14 +0300

    add page stars

---
+++ src/pages/Stars.vue
@@ -0,0 +1,21 @@
+<script setup>
+import meatGenIcon from "@/assets/img/icon/1-3.png";
+
+definePage({
+  path: '/stars',
+  name: 'Звезды',
+  meta: {
+    title: 'Звезды',
+    sidebar: true,
+    groups: ['cheatsheet'],
+  },
+});
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="sass">
+
+</style>
