fd82c384b8650d4d76524ad2ada30fed4ed8e9c3
enjoys <deadly.info@gmail.com>
2026-02-08 21:54:04 +0300

    add page harmony

---
+++ src/pages/Harmony.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+definePage({
+  path: '/harmony',
+  name: 'Harmony',
+  meta: {
+    title: 'Гармония',
+    sidebar: true,
+    groups: ['cheatsheet'],
+    extendedTitle: '"Гармошка" советников',
+  },
+});
+</script>
+
+<template>
+
+  <v-list>
+    <v-list-item>Варвары (Суб, Хуб, Нок, ...)</v-list-item>
+    <v-list-item>Воительницы</v-list-item>
+    <v-list-item>Дочери вечности</v-list-item>
+    <v-list-item>Марко Поло</v-list-item>
+    <v-list-item>Кен</v-list-item>
+    <v-list-item>Томоэ Годзэн</v-list-item>
+    <v-list-item>Вилма</v-list-item>
+    <v-list-item>Джаянти</v-list-item>
+    <v-list-item>Сноу</v-list-item>
+    <v-list-item>Атилла</v-list-item>
+  </v-list>
+
+</template>
+
+<style scoped lang="sass">
+
+</style>
